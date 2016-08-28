ade7b4a604adfeb12c027f83393bd8dec98653f3
Erich Küster <erich.kuester@arcor.de>
2016-08-28 23:28:50 +0200

    Revised  calculation of moon phase
    correction of coefficients and comments

--- Moon Phase View/MoonPhase.swift
+++ Moon Phase View/MoonPhase.swift
@@ -2,7 +2,11 @@
 //  MoonPhase.swift
 //  Moon Phase View
 //
-//  Created by Erich Küster on 23.08.16.
+//  Calculations following Chapronts moon theory ELP-2000/82
+//  plus Bretagnons and Francous theory VSOP87 for the sun
+//  algorithms given by Jean Meeus
+//
+//  Created by Erich Küster on August 23, 2016
 //  Copyright © 2016 Erich Küster. All rights reserved.
 //
 
@@ -14,127 +18,206 @@ class MoonPhase: NSObject {
     var eventBegin = NSDate()
     var localizedEventBegin = ""
     var eventName: String = ""
-    
+    //  2451550.09166 + 29.53O58886t.t+0.00015437..
+    let moonPhaseData =  [
+        [2451550.09766, 29.530588861, 0.00015437,  0.00000015,  0.00000000073],
+        [      2.1534,  29.10535669, -0.0000014,  -0.00000011,  0],
+        [    201.5643, 385.81693528,  0.0107582,   0.00001238, -0.000000058],
+        [    160.7108, 390.67050284, -0.0016118,  -0.00000227,  0.000000011],
+        [    124.7746,  -1.56375580,  0.0020691,   0.00000215,  0]
+    ]
+    // coefficients for new moon, full moon, first/last quarter
+    let moonPhaseCoeffs = [
+        [-0.40720, -0.40614, -0.62801],
+        [ 0.17241,  0.17302,  0.17172],
+        [ 0.01608,  0.01614, -0.01183],
+        [ 0.01039,  0.01043,  0.00862],
+        [ 0.00739,  0.00734,  0.00804],
+        [-0.00514, -0.00515,  0.00454],
+        [ 0.00208,  0.00209,  0.00204],
+        [-0.00111, -0.00111, -0.00180],
+        [-0.00057, -0.00057, -0.00070],
+        [ 0.00056,  0.00056, -0.00040],
+        [-0.00042, -0.00042, -0.00034],
+        [ 0.00042,  0.00042,  0.00032],
+        [ 0.00038,  0.00038,  0.00032],
+        [-0.00024, -0.00024, -0.00028],
+        [-0.00017, -0.00017,  0.00027],
+        [-0.00007, -0.00007, -0.00017],
+        [ 0.00004,  0.00004, -0.00005],
+        [ 0.00004,  0.00004,  0.00004],
+        [ 0.00003,  0.00003, -0.00004],
+        [ 0.00003,  0.00003,  0.00004],
+        [-0.00003, -0.00003,  0.00003],
+        [ 0.00003,  0.00003,  0.00003],
+        [-0.00002, -0.00002,  0.00002],
+        [-0.00002, -0.00002,  0.00002],
+        [ 0.00002,  0.00002, -0.00002],
+    ]
+    // Coefficients of A1 - A14.
+    let moonPhaseExtra = [
+        [299.77,  0.107408, 0.000325],
+        [251.88,  0.016321, 0.000165],
+        [251.83, 26.651886, 0.000164],
+        [349.42, 36.412478, 0.000126],
+        [ 84.66, 18.206239, 0.000110],
+        [141.74, 53.303771, 0.000062],
+        [207.14,  2.153732, 0.000060],
+        [154.84,  7.306860, 0.000056],
+        [ 34.52, 27.261239, 0.000047],
+        [207.19,  0.121824, 0.000042],
+        [291.34,  1.844379, 0.000040],
+        [161.72, 24.198154, 0.000037],
+        [239.56, 25.513099, 0.000035],
+        [331.55,  2.592518, 0.000023]
+    ]
+    let pi2 = 2.0 * M_PI
+    let rads: Double = 0.0174532925199433
+
     init(luna: Int, ph: Int) {
         lunation = luna
         phase = ph
     }
-    
+
     func sortDatesAscending (moonPhase: MoonPhase) -> Bool {
         // NSOrderedAscending = -1, NSOrderedSame, NSOrderedDescending
         return (eventBegin.compare(moonPhase.eventBegin) == NSComparisonResult.OrderedAscending)
     }
-    
-    func correctMoon(coeffs: Array<Double>, E: Double, F: Double, M: Double, MStrich: Double, Omega: Double) -> Double {
-        var add = coeffs[0] * sin(MStrich)
-        add = add + coeffs[1] * E * sin(M)
-        add = add + coeffs[2] * sin(2 * MStrich)
-        add = add + coeffs[3] * sin(2 * F)
-        add = add + coeffs[4] * E * sin(MStrich - M)
-        add = add + coeffs[5] * E * sin(MStrich + M)
-        add = add + coeffs[6] * E * E * sin(2 * M)
-        add = add + coeffs[7] * sin(MStrich - 2 * F)
-        add = add + coeffs[8] * sin(MStrich + 2 * F)
-        add = add + coeffs[9] * E * sin(2 * MStrich + M)
-        add = add + coeffs[10] * sin(3 * MStrich)
-        add = add + coeffs[11] * E * sin(M + 2 * F)
-        add = add + coeffs[12] * E * sin(M - 2 * F)
-        add = add + coeffs[13] * E * sin(2 * MStrich - M)
-        add = add + coeffs[14] * sin(Omega)
-        add = add + coeffs[15] * sin(MStrich + 2 * M)
-        add = add + coeffs[16] * sin(2 * MStrich - 2 * F)
-        add = add + coeffs[17] * sin(3 * M)
-        add = add + coeffs[18] * sin(MStrich + M - 2 * F)
-        add = add + coeffs[19] * sin(2 * MStrich + 2 * F)
-        add = add + coeffs[20] * sin(MStrich + M + 2 * F)
-        add = add + coeffs[21] * sin(MStrich - M + 2 * F)
-        add = add + coeffs[22] * sin(MStrich - M - 2 * F)
-        add = add + coeffs[23] * sin(3 * MStrich + M)
-        add = add + coeffs[24] * sin(4 * MStrich)
+
+    func evalMoonPhaseData(n: Int, k: Double, T: Double) -> Double {
+        var sum = moonPhaseData[n][0] + k * moonPhaseData[n][1]
+        let m2 = moonPhaseData[n][2]
+        let m3 = moonPhaseData[n][3]
+        let m4 = moonPhaseData[n][4]
+        sum = sum + T * T * (m2 + T * (m3 + T * m4))
+        if n > 0 {
+            sum *= rads
+            // reduce angle in radians to the interval 0 ... 2pi
+//            sum -= floor (sum / pi2) * pi2
+        }
+        return sum
+    }
+
+    func correctMoon(row: Int, E: Double, F: Double, M: Double, MStrich: Double, Omega: Double) -> Double {
+        // corrections for new resp. full moon
+        var add = moonPhaseCoeffs[0][row] * sin(MStrich)
+        add = add + moonPhaseCoeffs[1][row] * E * sin(M)
+        add = add + moonPhaseCoeffs[2][row] * sin(2.0 * MStrich)
+        add = add + moonPhaseCoeffs[3][row] * sin(2.0 * F)
+        add = add + moonPhaseCoeffs[4][row] * E * sin(MStrich - M)
+        add = add + moonPhaseCoeffs[5][row] * E * sin(MStrich + M)
+        add = add + moonPhaseCoeffs[6][row] * E * E * sin(2.0 * M)
+        add = add + moonPhaseCoeffs[7][row] * sin(MStrich - 2.0 * F)
+        add = add + moonPhaseCoeffs[8][row] * sin(MStrich + 2.0 * F)
+        add = add + moonPhaseCoeffs[9][row] * E * sin(2.0 * MStrich + M)
+        add = add + moonPhaseCoeffs[10][row] * sin(3.0 * MStrich)
+        add = add + moonPhaseCoeffs[11][row] * E * sin(M + 2.0 * F)
+        add = add + moonPhaseCoeffs[12][row] * E * sin(M - 2.0 * F)
+        add = add + moonPhaseCoeffs[13][row] * E * sin(2.0 * MStrich - M)
+        add = add + moonPhaseCoeffs[14][row] * sin(Omega)
+        add = add + moonPhaseCoeffs[15][row] * sin(MStrich + 2.0 * M)
+        add = add + moonPhaseCoeffs[16][row] * sin(2.0 * MStrich - 2.0 * F)
+        add = add + moonPhaseCoeffs[17][row] * sin(3.0 * M)
+        add = add + moonPhaseCoeffs[18][row] * sin(MStrich + M - 2.0 * F)
+        add = add + moonPhaseCoeffs[19][row] * sin(2.0 * MStrich + 2.0 * F)
+        add = add + moonPhaseCoeffs[20][row] * sin(MStrich + M + 2.0 * F)
+        add = add + moonPhaseCoeffs[21][row] * sin(MStrich - M + 2.0 * F)
+        add = add + moonPhaseCoeffs[22][row] * sin(MStrich - M - 2.0 * F)
+        add = add + moonPhaseCoeffs[23][row] * sin(3.0 * MStrich + M)
+        add = add + moonPhaseCoeffs[24][row] * sin(4 * MStrich)
         return add
     }
     
-    func correctQuarter(coeffs: Array<Double>, E: Double, F: Double, M: Double, MStrich: Double, Omega: Double, sign:Int) -> Double {
+    func correctQuarter(row: Int, E: Double, F: Double, M: Double, MStrich: Double, Omega: Double) -> Double {
         // corrections for first resp. last moon quarter
-        var add = coeffs[0] * sin(MStrich)
-        add = add + coeffs[1] * E * sin(M)
-        add = add + coeffs[2] * E * sin(MStrich + M)
-        add = add + coeffs[3] * sin(2 * MStrich)
-        add = add + coeffs[4] * sin(2 * F)
-        add = add + coeffs[5] * E * sin(MStrich - M)
-        add = add + coeffs[6] * E * E * sin(2 * M)
-        add = add + coeffs[7] * sin(MStrich - 2 * F)
-        add = add + coeffs[8] * sin(MStrich + 2 * F)
-        add = add + coeffs[9] * sin(3 * MStrich)
-        add = add + coeffs[10] * E * sin(2 * MStrich - M)
-        add = add + coeffs[11] * E * sin(M + 2 * F)
-        add = add + coeffs[12] * E * sin(M - 2 * F)
-        add = add + coeffs[13] * E * E * sin(MStrich + 2 * M)
-        add = add + coeffs[14] * E * sin(2 * MStrich + M)
-        add = add + coeffs[15] * sin(Omega)
-        add = add + coeffs[16] * sin(MStrich - M - 2 * F)
-        add = add + coeffs[17] * sin(2 * MStrich + 2 * F)
-        add = add + coeffs[18] * sin(MStrich + M + 2 * F)
-        add = add + coeffs[19] * sin(MStrich - 2 * M)
-        add = add + coeffs[20] * sin(MStrich + M - 2 * F)
-        add = add + coeffs[21] * sin(3 * M)
-        add = add + coeffs[22] * sin(2 * MStrich - 2 * F)
-        add = add + coeffs[23] * sin(MStrich - M + 2 * F)
-        add = add + coeffs[24] * sin(3 * MStrich + M)
-        var W = 0.00306 - 0.00038 * E * Double(cos(M))
-        W += 0.00026 * Double(cos(MStrich))
-        W -= 2e-05 * Double(cos(MStrich - M))
-        W += 2e-05 * Double(cos(MStrich + M))
-        W += 2e-05 * Double(cos(2 * F))
-        add = add + Double(sign) * W
+        var add = moonPhaseCoeffs[0][row] * sin(MStrich)
+        add = add + moonPhaseCoeffs[1][row] * E * sin(M)
+        add = add + moonPhaseCoeffs[2][row] * E * sin(MStrich + M)
+        add = add + moonPhaseCoeffs[3][row] * sin(2.0 * MStrich)
+        add = add + moonPhaseCoeffs[4][row] * sin(2.0 * F)
+        add = add + moonPhaseCoeffs[5][row] * E * sin(MStrich - M)
+        add = add + moonPhaseCoeffs[6][row] * E * E * sin(2.0 * M)
+        add = add + moonPhaseCoeffs[7][row] * sin(MStrich - 2.0 * F)
+        add = add + moonPhaseCoeffs[8][row] * sin(MStrich + 2.0 * F)
+        add = add + moonPhaseCoeffs[9][row] * sin(3.0 * MStrich)
+        add = add + moonPhaseCoeffs[10][row] * E * sin(2.0 * MStrich - M)
+        add = add + moonPhaseCoeffs[11][row] * E * sin(M + 2.0 * F)
+        add = add + moonPhaseCoeffs[12][row] * E * sin(M - 2.0 * F)
+        add = add + moonPhaseCoeffs[13][row] * E * E * sin(MStrich + 2.0 * M)
+        add = add + moonPhaseCoeffs[14][row] * E * sin(2.0 * MStrich + M)
+        add = add + moonPhaseCoeffs[15][row] * sin(Omega)
+        add = add + moonPhaseCoeffs[16][row] * sin(MStrich - M - 2.0 * F)
+        add = add + moonPhaseCoeffs[17][row] * sin(2.0 * MStrich + 2.0 * F)
+        add = add + moonPhaseCoeffs[18][row] * sin(MStrich + M + 2.0 * F)
+        add = add + moonPhaseCoeffs[19][row] * sin(MStrich - 2.0 * M)
+        add = add + moonPhaseCoeffs[20][row] * sin(MStrich + M - 2.0 * F)
+        add = add + moonPhaseCoeffs[21][row] * sin(3.0 * M)
+        add = add + moonPhaseCoeffs[22][row] * sin(2.0 * MStrich - 2.0 * F)
+        add = add + moonPhaseCoeffs[23][row] * sin(MStrich - M + 2.0 * F)
+        add = add + moonPhaseCoeffs[24][row] * sin(3.0 * MStrich + M)
         return add
     }
-    
+
+    func evalW(E: Double, F: Double, M: Double, MStrich: Double) -> Double {
+        var w = 0.00306 - 0.00038 * E * cos(M)
+        w += 0.00026 * cos(MStrich)
+        w -= 0.00002 * cos(MStrich - M)
+        w += 0.00002 * cos(MStrich + M)
+        w += 0.00002 * cos(2.0 * F)
+        return w
+    }
+
     func eventBeginPastJulianDay(jd: Double) -> Void {
         // next moon phase after julian day
         // from the Book Jean Meeus, Astronomische Algorithmen, p. 348 ff.
-        let rads: Double = 0.0174532925199433
-        let nmCoeffList: Array<Double> = [-0.4072, 0.17241, 0.01608, 0.01039, 0.00739, -0.00514, 0.00208, -0.00111, -0.00057, 0.00056, -0.00042, -0.00042, 0.00038, -0.00024, -0.00017, -7e-05, 4e-05, 4e-05, 3e-05, 3e-05, -3e-05, 3e-05, -2e-05, -2e-05, 2e-05]
-        let fmCoeffList: Array<Double> = [-0.40614, 0.17302, 0.01614, 0.01043, 0.00734, -0.00515, 0.00209, -0.00111, -0.00057, 0.00056, -0.00042, 0.00042, 0.00038, -0.00024, -0.00017, -7e-05, 4e-05, 4e-05, 3e-05, 3e-05, -3e-05, 3e-05, -2e-05, -2e-05, 2e-05]
-        let quCoeffList: Array<Double> = [-0.62801, 0.17172, -0.01183, 0.00862, 0.00804, 0.00454, 0.00204, -0.0018, -0.0007, -0.0004, -0.00034, 0.00032, 0.00032, -0.00028, 0.00027, -0.00017, -5e-05, 4e-05, -4e-05, 4e-05, 3e-05, 3e-05, 2e-05, 2e-05, -2e-05]
         // lunations since new moon on january 6, 2000
-        // k = (jd + 1 - 2451550.09765) / 29.530588853;
         let k: Double = (jd - 2451550.09765) / 29.530588853
         let k0: Double = floor(k) + Double(lunation) + Double(phase) * 0.25
-        let T = k0 / 1236.85
-        let T2 = T * T
-        let T3 = T2 * T
-        let T4 = T2 * T2
-        let E = 1 - 0.002516 * T - 7.4e-06 * T2
-        // mean anomaly of sun at time jde
-        let M = (2.5534 + 29.10535669 * k0 - 2.18e-05 * T2 - 1.1e-07 * T3) * rads
-        // mean anomaly of moon at time jde
-        let MStrich = (201.5643 + 385.81693528 * k0 + 0.1017438 * T2 + 1.239e-05 * T3 - 5.8e-08 * T4) * rads
-        // Argument der Breite des Mondes
-        let F = (160.7108 + 390.67050274 * k0 - 0.0016341 * T2 - 2.27e-06 * T3 + 1.1e-08 * T4) * rads
-        // longitude of the ascending node in lunar orbit
-        let Omega = (124.7746 - 1.5637558 * k0 + 0.002091 * T2 + 2.15e-06 * T3) * rads
-        let jde = 2451550.09765 + 29.530588853 * k0 + 0.0001337 * T2 - 1.5e-07 * T3 + 7.3e-10 * T4
-        var add: Double = 0.0
+        let T = k0 / 1236.82664
+        // excentricity of the orbit of earth
+        let E = 1 - T * (0.002516 + T * 0.0000074)
+        // julian ephemeris day, read more at <https://en.wikipedia.org/wiki/Terrestrial_Time>
+        let jde = evalMoonPhaseData(0, k: k0, T: T)
+        // mean anomaly of the sun at time jde
+        let M = evalMoonPhaseData(1, k: k0, T: T)
+        // mean anomaly of the moon at time jde
+        let MStrich = evalMoonPhaseData(2, k: k0, T: T)
+        // moon's argument of latitude
+        let F = evalMoonPhaseData(3, k: k0, T: T)
+        // longitude of the ascending node of the lunar orbit
+        let Omega = evalMoonPhaseData(4, k: k0, T: T)
+        // apply perturbation terms due to sun and planets
+        // first the sun
+        var sun: Double = 0.0
         switch (phase) {
         case 0:
             eventName = NSLocalizedString("new moon", comment: "first moon phase")
-            add = correctMoon(nmCoeffList, E: E, F: F, M: M, MStrich: MStrich, Omega: Omega)
+            sun = correctMoon(0, E: E, F: F, M: M, MStrich: MStrich, Omega: Omega)
         case 1:
             eventName = NSLocalizedString("crescent moon", comment: "second moon phase")
-            add = correctQuarter(quCoeffList, E: E, F: F, M: M, MStrich: MStrich, Omega: Omega, sign: 1)
+            sun = correctQuarter(2, E: E, F: F, M: M, MStrich: MStrich, Omega: Omega)
+            sun = sun + evalW(E, F: F, M: M, MStrich: MStrich)
         case 2:
             eventName = NSLocalizedString("full moon", comment: "third moon phase")
-            add = correctMoon(fmCoeffList, E: E, F: F, M: M, MStrich: MStrich, Omega: Omega)
+            sun = correctMoon(1, E: E, F: F, M: M, MStrich: MStrich, Omega: Omega)
         case 3:
             eventName = NSLocalizedString("waning moon", comment: "last moon phase")
-            add = correctQuarter(quCoeffList, E: E, F: F, M: M, MStrich: MStrich, Omega: Omega, sign: -1)
+            sun = correctQuarter(2, E: E, F: F, M: M, MStrich: MStrich, Omega: Omega)
+            sun = sun - evalW(E, F: F, M: M, MStrich: MStrich)
         default:
             Swift.print(NSLocalizedString("Phase not between 0 and 3", comment: "no real moon phase"))
-            
         }
-        // beginning of event, expressed in seconds relative to 1. january 2001 as NSDate object
-        eventBegin = NSDate(timeIntervalSinceReferenceDate: (jde + add - 2451910.5) * 86400)
+        // extra perturbations due to planets
+        var planets = 0.0
+        var A: [Double] = []
+        for i in 0 ..< 14 {
+            A.append(moonPhaseExtra[i][0] + k0 * moonPhaseExtra[i][1])
+        }
+        A[0] -= 0.009173 * T * T
+        for i in 0 ..< 14 {
+            planets += moonPhaseExtra[i][2] * sin(A[i] * rads)
+        }
+        // beginning of event, expressed in seconds relative to January 1, 2001 as NSDate object
+        eventBegin = NSDate(timeIntervalSinceReferenceDate: (jde + sun + planets - 2451910.5) * 86400)
     }
 }
-
